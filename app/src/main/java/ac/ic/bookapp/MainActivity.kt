5500b7be40eaf544fbd77d263cb23368bcdd027c
Anand Doshi <anand.doshi20@imperial.ac.uk>
2022-06-09 14:34:17 +0100

    [ad4220] [feat] Added a borrow button and accompanying message.

--- app/src/main/java/ac/ic/bookapp/MainActivity.kt
+++ app/src/main/java/ac/ic/bookapp/MainActivity.kt
@@ -2,10 +2,17 @@ package ac.ic.bookapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    val button1: Button = findViewById<Button>(R.id.button6)
+    button1.setOnClickListener {
+      val borrowToast = Toast.makeText(this, "Book Borrowed", Toast.LENGTH_SHORT)
+      borrowToast.show()
+    }
   }
 }
