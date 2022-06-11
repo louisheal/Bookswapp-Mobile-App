a20abdb87d7a5a311a7eaf9272cbf3814ed52ffc
Anand Doshi <anand.doshi20@imperial.ac.uk>
2022-06-11 12:05:03 +0100

    [ad4220] [refactor] Made helper methods private.

--- app/src/main/java/ac/ic/bookapp/MainActivity.kt
+++ app/src/main/java/ac/ic/bookapp/MainActivity.kt
@@ -15,129 +15,131 @@ import retrofit2.http.*
 
 class MainActivity : AppCompatActivity() {
 
-  private val BORROW: String = "Duplicate"
-  private var currentID: Int = 5
+    private val BORROW: String = "Duplicate"
+    private var currentID: Int = 5
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    val response : List<Book> = getBooks()
-    displayBooks(response)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val response: List<Book> = getBooks()
+        displayBooks(response)
+    }
 
-  fun createRow(book: Book): TableRow {
-    val row = TableRow(this)
-    val text = TextView(this)
-    text.setText(book.title)
-    row.addView(text)
-    val button = Button(this)
-    button.setText(BORROW)
-    button.setOnClickListener {
+    private fun createRow(book: Book): TableRow {
+        val row = TableRow(this)
+        val text = TextView(this)
+        text.setText(book.title)
+        row.addView(text)
+        val button = Button(this)
+        button.setText(BORROW)
+        button.setOnClickListener {
 //      val borrowToast = Toast.makeText(this, "Book Duplicated", Toast.LENGTH_SHORT)
 //      borrowToast.show()
-      book.id = currentID.toString()
-      currentID++
-      borrowBook(book)
+            book.id = currentID.toString()
+            currentID++
+            borrowBook(book)
+        }
+        row.addView(button)
+        return row
     }
-    row.addView(button)
-    return row
-  }
 
-  private fun borrowBook(book: Book) {
+    private fun borrowBook(book: Book) {
 //    val table = findViewById<TableLayout>(R.id.table)
 //    val row = createRow(book)
 //    table.addView(row)
-    runBlocking {
-      try {
-        val response = BookApi.retrofitService.postBook(JBook("5", "new book", "today"))
-        val toast = Toast.makeText(this@MainActivity,
-          response.code().toString(), Toast.LENGTH_SHORT)
-        toast.show()
-      } catch (e: Exception) {
-        e.printStackTrace()
-      }
+        runBlocking {
+            try {
+                val response = BookApi.retrofitService.postBook(JBook("5", "new book", "today"))
+                val toast = Toast.makeText(
+                    this@MainActivity,
+                    response.code().toString(), Toast.LENGTH_SHORT
+                )
+                toast.show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
-  }
 
-  fun displayBooks(books: List<Book>) {
-    val table = findViewById<TableLayout>(R.id.table)
-    table.removeAllViews()
-    for (book in books) {
-      val row = createRow(book)
-      table.addView(row)
+    private fun displayBooks(books: List<Book>) {
+        val table = findViewById<TableLayout>(R.id.table)
+        table.removeAllViews()
+        for (book in books) {
+            val row = createRow(book)
+            table.addView(row)
+        }
     }
-  }
 
-  private fun getBooks(): List<Book> {
-    var response : List<Book> = listOf()
-    runBlocking {
-      try {
-        response = BookApi.retrofitService.getBooks()
-      } catch (e: Exception) {
-        e.printStackTrace()
-      }
+    private fun getBooks(): List<Book> {
+        var response: List<Book> = listOf()
+        runBlocking {
+            try {
+                response = BookApi.retrofitService.getBooks()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return response
     }
-    return response
-  }
 
-  private fun getUsers(): List<User> {
-    var response : List<User> = listOf()
-    runBlocking {
-      try {
-        response = BookApi.retrofitService.getUsers()
-      } catch (e: Exception) {
-        e.printStackTrace()
-      }
+    private fun getUsers(): List<User> {
+        var response: List<User> = listOf()
+        runBlocking {
+            try {
+                response = BookApi.retrofitService.getUsers()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return response
     }
-    return response
-  }
 }
 
 private val moshi = Moshi.Builder()
-  .add(KotlinJsonAdapterFactory())
-  .build()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 val retrofit = Retrofit.Builder()
-  .baseUrl("http://146.169.43.115:8080/")
-  .addConverterFactory(MoshiConverterFactory.create(moshi))
-  .build()
+    .baseUrl("http://146.169.43.115:8080/")
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .build()
 
 interface BookApiService {
-  @GET("books")
-  suspend fun getBooks() : List<Book>
+    @GET("books")
+    suspend fun getBooks(): List<Book>
 
-  @GET("users")
-  suspend fun getUsers() : List<User>
+    @GET("users")
+    suspend fun getUsers(): List<User>
 
-  @Headers("Content-Type: application/json")
-  @POST("books")
-  suspend fun postBook(@Body book : JBook) : Response<Unit>
+    @Headers("Content-Type: application/json")
+    @POST("books")
+    suspend fun postBook(@Body book: JBook): Response<Unit>
 }
 
 object BookApi {
-  val retrofitService: BookApiService by lazy { retrofit.create(BookApiService::class.java) }
+    val retrofitService: BookApiService by lazy { retrofit.create(BookApiService::class.java) }
 }
 
 data class Book(
-  @field:Json(name = "id") var id: String,
-  @field:Json(name = "isbn") val isbn: String,
-  @field:Json(name = "title") val title: String,
-  @field:Json(name = "published") val published: String
+    @field:Json(name = "id") var id: String,
+    @field:Json(name = "isbn") val isbn: String,
+    @field:Json(name = "title") val title: String,
+    @field:Json(name = "published") val published: String
 )
 
 @JsonClass(generateAdapter = true)
 data class JBook(
-  @field:Json(name = "isbn") val isbn: String,
-  @field:Json(name = "title") val title: String,
-  @field:Json(name = "published") val published: String
+    @field:Json(name = "isbn") val isbn: String,
+    @field:Json(name = "title") val title: String,
+    @field:Json(name = "published") val published: String
 )
 
 data class User(
-  val id: String,
-  val username: String,
-  val passwdHash: String,
-  val name: String,
-  val email: String,
-  val phone: String,
-  val joinDate: String
+    val id: String,
+    val username: String,
+    val passwdHash: String,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val joinDate: String
 )
