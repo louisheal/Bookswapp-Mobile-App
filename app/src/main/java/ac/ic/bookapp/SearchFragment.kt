258de3c2e92409b769ef2d0a81c0b33e42ed7f32
Anand Doshi <anand.doshi20@imperial.ac.uk>
2022-06-14 16:40:15 +0100

    [ad4220] [feat] Added scrollable list of borrowed books to search page

--- app/src/main/java/ac/ic/bookapp/SearchFragment.kt
+++ app/src/main/java/ac/ic/bookapp/SearchFragment.kt
@@ -1,18 +1,24 @@
 package ac.ic.bookapp
 
+import ac.ic.bookapp.adaptors.BorrowBookRowAdapter
+import ac.ic.bookapp.data.Datasource
 import ac.ic.bookapp.databinding.FragmentMyBooksBinding
 import ac.ic.bookapp.databinding.FragmentSearchBinding
+import ac.ic.bookapp.model.Book
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
 
     private val binding get() = _binding!!
+
+    private lateinit var scrollableList: RecyclerView
 
     @Override
     override fun onCreateView(
@@ -22,7 +28,18 @@ class SearchFragment : Fragment() {
     ): View? {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        scrollableList = binding.searchList
+        scrollableList.setHasFixedSize(true)
+
         return view
+    }
+
+    @Override
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // val books = listOf<Book>(Book("1","2", "Title", "2022-06-14"))
+        displayBorrowableBooks(Datasource.getBooks())
     }
 
     @Override
@@ -30,4 +47,11 @@ class SearchFragment : Fragment() {
         super.onDestroy()
         _binding = null
     }
+
+    private fun displayBorrowableBooks(books: List<Book>) {
+        val adapter = activity?.let { BorrowBookRowAdapter(it, books) }
+        scrollableList.adapter = adapter
+    }
+
+
 }
---
+++ app/src/main/java/ac/ic/bookapp/adaptors/BorrowBookRowAdapter.kt
@@ -0,0 +1,47 @@
+package ac.ic.bookapp.adaptors
+
+import ac.ic.bookapp.R
+import ac.ic.bookapp.model.Book
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class BorrowBookRowAdapter(private val context: Context,
+                           private val booksList: List<Book>):
+    RecyclerView.Adapter<BorrowBookRowAdapter.BorrowBookRowViewHolder>() {
+
+    class BorrowBookRowViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val bookTitle: TextView = view.findViewById(R.id.my_books_book_text)
+        val borrowButton =  view.findViewById<Button>(R.id.borrow_button)
+        lateinit var book: Book
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BorrowBookRowViewHolder {
+        val adapterView = LayoutInflater.from(parent.context).inflate(R.layout.search_book_row, parent, false)
+        return BorrowBookRowViewHolder(adapterView)
+    }
+
+    override fun onBindViewHolder(holder: BorrowBookRowViewHolder, position: Int) {
+        val book = booksList[position]
+        holder.bookTitle.text = book.title
+        holder.book = book
+
+        holder.borrowButton.setOnClickListener {
+            val toast = Toast.makeText(
+                context,
+                book.title + " borrowed!", Toast.LENGTH_LONG
+            )
+            toast.show()
+        }
+    }
+
+    override fun getItemCount(): Int = booksList.size
+}
